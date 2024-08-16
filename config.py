452b0fd1b0ef7724a4a97c0562fc74e032e77880
Diven <Divenmail07@gmail.com>
2024-08-17 01:48:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TeamInflex")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/pfp_st0re")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/InflexSupport")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
